ec21db47fe5f17527acebc0a7e8c2f4fba8a921e
SeMe4K-0 <150553644+SeMe4K-0@users.noreply.github.com>
2026-03-06 15:59:44 +0300

    Correct misstake

--- cameras-project/vite.config.ts
+++ cameras-project/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/power-consumption-of-cameras-tauri/',
   server: { 
     port: 3000,
     proxy: {
